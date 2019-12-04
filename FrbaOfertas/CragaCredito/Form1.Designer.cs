9fcf1483fc80e3401ec9dbf0714cc5b455711344
Damuchi99 <damuchi99@gmail.com>
2019-12-04 02:27:55 -0300

    Hago vista de Carga de Creditos, he comentado algunas cosas que faltan pulir

--- FrbaOfertas/CragaCredito/Form1.Designer.cs
+++ FrbaOfertas/CragaCredito/Form1.Designer.cs
@@ -28,11 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.monto = new System.Windows.Forms.TextBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.tipopago = new System.Windows.Forms.TextBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monto
+            // 
+            this.monto.Location = new System.Drawing.Point(16, 110);
+            this.monto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.monto.Name = "monto";
+            this.monto.Size = new System.Drawing.Size(345, 22);
+            this.monto.TabIndex = 32;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(13, 22);
+            this.label20.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(47, 17);
+            this.label20.TabIndex = 31;
+            this.label20.Text = "monto";
+            // 
+            // tipopago
+            // 
+            this.tipopago.Location = new System.Drawing.Point(16, 43);
+            this.tipopago.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tipopago.Name = "tipopago";
+            this.tipopago.Size = new System.Drawing.Size(345, 22);
+            this.tipopago.TabIndex = 30;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(13, 89);
+            this.label21.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(63, 17);
+            this.label21.TabIndex = 29;
+            this.label21.Text = "tipopago";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 158);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 87);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "agregar carga";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.monto);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.tipopago);
+            this.Controls.Add(this.label21);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox monto;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.TextBox tipopago;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Button button1;
     }
 }
--- FrbaOfertas/CragaCredito/Form1.cs
+++ FrbaOfertas/CragaCredito/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,41 @@ namespace FrbaOfertas.CragaCredito
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var command = new SqlCommand("INSERT INTO Cliente " +
+                "(Carga_Credito,Carga_Fecha,Tipo_Pago_Desc) " +
+                "VALUES (@monto,@fechaCredito,@tipopago)", Program.con);
+            command.Parameters.AddWithValue("@monto", monto.Text);
+            //command.Parameters.AddWithValue("@fechaCredito", fechacredito.Text);
+            //La fecha tiene que ser del archivo de configuracion de la aplicacion
+            command.Parameters.AddWithValue("@tipopago", tipopago.Text);
+            //el tipo de pago tiene que salir de la tabla 
+
+            //para agregar el id de cliente habra que hacer un join?
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException er)
+            {
+                var newForm = new ErrorWindow();
+
+                Console.WriteLine(er.Message + " >>>>>>>" + er.Number);
+                if (er.Number == 2627)
+                {
+                    newForm.setText("Un credito con esos datos ya existe");
+                }
+                else
+                {
+                    newForm.setText("Datos faltantes o mal ingresados");
+                }
+
+                newForm.Show();
+            }
+            Close();
         }
     }
 }
