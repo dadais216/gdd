b45c26b74230b2b117f520d48dc53f73f942a686
Ambro17 <ambro17@gmail.com>
2019-12-06 21:39:20 -0300

    First layout

--- FrbaOfertas/ListadoEstadistico/Form1.Designer.cs
+++ FrbaOfertas/ListadoEstadistico/Form1.Designer.cs
@@ -28,11 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BestRent = new System.Windows.Forms.Button();
+            this.bestValue = new System.Windows.Forms.Button();
+            this.TablaListado = new System.Windows.Forms.DataGridView();
+            this.selectSemestre = new System.Windows.Forms.Label();
+            this.selectSemestreCombo = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TablaListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BestRent
+            // 
+            this.BestRent.Location = new System.Drawing.Point(1183, 161);
+            this.BestRent.Name = "BestRent";
+            this.BestRent.Size = new System.Drawing.Size(350, 76);
+            this.BestRent.TabIndex = 0;
+            this.BestRent.Text = "Best Rent";
+            this.BestRent.UseVisualStyleBackColor = true;
+            // 
+            // bestValue
+            // 
+            this.bestValue.Location = new System.Drawing.Point(55, 161);
+            this.bestValue.Name = "bestValue";
+            this.bestValue.Size = new System.Drawing.Size(350, 76);
+            this.bestValue.TabIndex = 1;
+            this.bestValue.Text = "Best Value";
+            this.bestValue.UseVisualStyleBackColor = true;
+            // 
+            // TablaListado
+            // 
+            this.TablaListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TablaListado.Location = new System.Drawing.Point(55, 274);
+            this.TablaListado.Name = "TablaListado";
+            this.TablaListado.RowTemplate.Height = 33;
+            this.TablaListado.Size = new System.Drawing.Size(1478, 502);
+            this.TablaListado.TabIndex = 2;
+            // 
+            // selectSemestre
+            // 
+            this.selectSemestre.AutoSize = true;
+            this.selectSemestre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectSemestre.Location = new System.Drawing.Point(64, 56);
+            this.selectSemestre.Name = "selectSemestre";
+            this.selectSemestre.Size = new System.Drawing.Size(322, 37);
+            this.selectSemestre.TabIndex = 3;
+            this.selectSemestre.Text = "Seleccionar semestre";
+            this.selectSemestre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // selectSemestreCombo
+            // 
+            this.selectSemestreCombo.FormattingEnabled = true;
+            this.selectSemestreCombo.Location = new System.Drawing.Point(815, 56);
+            this.selectSemestreCombo.Name = "selectSemestreCombo";
+            this.selectSemestreCombo.Size = new System.Drawing.Size(718, 33);
+            this.selectSemestreCombo.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1619, 834);
+            this.Controls.Add(this.selectSemestreCombo);
+            this.Controls.Add(this.selectSemestre);
+            this.Controls.Add(this.TablaListado);
+            this.Controls.Add(this.bestValue);
+            this.Controls.Add(this.BestRent);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TablaListado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BestRent;
+        private System.Windows.Forms.Button bestValue;
+        private System.Windows.Forms.DataGridView TablaListado;
+        private System.Windows.Forms.Label selectSemestre;
+        private System.Windows.Forms.ComboBox selectSemestreCombo;
     }
 }
