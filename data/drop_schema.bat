38afcd733c386b86b7e82ba6ae8ed5b76819e219
Ambro17 <ambro17@gmail.com>
2019-12-13 07:13:01 -0300

    Change database username as locally is gd for me, but it is required to be named gdCupon2019

--- data/drop_schema.bat
+++ data/drop_schema.bat
@@ -1,1 +1,1 @@
-sqlcmd -S localhost\SQLSERVER2012 -U gd -P gd2019 -i drop_schema.sql -a 32767  -o log_drop.txt
+sqlcmd -S localhost\SQLSERVER2012 -U gdCupon2019 -P gd2019 -i drop_schema.sql -a 32767  -o log_drop.txt
